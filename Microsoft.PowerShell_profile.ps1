c37ca303f1b1b46f6fc06189c5330a8002f2d0f3
Tomisall <tsheridan99@gmail.com>
2023-05-11 14:29:21 +0100

    Update Microsoft.PowerShell_profile.ps1
    updated 11 May 2023

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -9,3 +9,18 @@ New-Alias choco 'C:\ProgramData\chocoportable\choco.exe'
 New-Alias vim 'C:\tools\vim\vim90\vim.exe'
 New-Alias edge Start-Process
 New-Alias pandoc 'C:\Users\EVO1264\AppData\Local\Pandoc\pandoc.exe'
+New-Alias fzf  'C:\ProgramData\chocolatey\lib\fzf\tools\fzf.exe'
+New-Alias word "C:\Program Files\Microsoft Office\root\Office16\WINWORD.EXE"
+function open() {
+	$param1=$args[0] 
+	
+	if ($args[0] -eq $null)
+	{
+		ii $(fzf)
+	}
+
+	else
+	{
+	ii $(fzf --query "$param1")
+	} 
+}
