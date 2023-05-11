121769b490bdd79c313b16ec3c55456e594ebc49
Tomisall <tsheridan99@gmail.com>
2023-05-11 14:31:12 +0100

    Update Microsoft.PowerShell_profile.ps1
    fix1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,6 @@
-function scripts { set-location "C:\Users\Evo1264\Documents\script" }
+function scripts { set-location "C:\Users\USER\Documents\script" }
 function bus { Start-Process "https://www.oxfordbus.co.uk/stops/3400030036" }
-function busstop { python "C:\Users\Evo1264\Documents\script\busstop.py" }
+function busstop { python "C:\Users\USER\Documents\script\busstop.py" }
 function bs { 
 	clear
 	busstop
@@ -8,7 +8,7 @@ function bs {
 New-Alias choco 'C:\ProgramData\chocoportable\choco.exe'
 New-Alias vim 'C:\tools\vim\vim90\vim.exe'
 New-Alias edge Start-Process
-New-Alias pandoc 'C:\Users\EVO1264\AppData\Local\Pandoc\pandoc.exe'
+New-Alias pandoc 'C:\Users\USER\AppData\Local\Pandoc\pandoc.exe'
 New-Alias fzf  'C:\ProgramData\chocolatey\lib\fzf\tools\fzf.exe'
 New-Alias word "C:\Program Files\Microsoft Office\root\Office16\WINWORD.EXE"
 function open() {
