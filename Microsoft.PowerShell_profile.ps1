c4f02d6c039a2dc1c589a403fcb95d344f6ea992
Tomisall <tsheridan99@gmail.com>
2023-05-12 12:52:30 +0100

    Create Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,40 @@
+function scripts { set-location "C:\Users\USER\Documents\script" }
+function bus { Start-Process "https://www.oxfordbus.co.uk/stops/3400030036" }
+function busstop { python "C:\Users\USER\Documents\script\busstop.py" }
+function bs { 
+	clear
+	busstop
+}
+New-Alias choco 'C:\ProgramData\chocoportable\choco.exe'
+New-Alias vim 'C:\tools\vim\vim90\vim.exe'
+New-Alias edge Start-Process
+New-Alias pandoc 'C:\Users\USER\AppData\Local\Pandoc\pandoc.exe'
+New-Alias fzf  'C:\ProgramData\chocolatey\lib\fzf\tools\fzf.exe'
+New-Alias word "C:\Program Files\Microsoft Office\root\Office16\WINWORD.EXE"
+function open() {
+	$param1=$args[0] 
+	
+	if ($args[0] -eq $null)
+	{
+		ii $(fzf)
+	}
+
+	else
+	{
+	ii $(fzf --query "$param1")
+	} 
+}
+function mdup() {
+	$param1=$args[0] 
+	$param2=$args[1] 
+
+	if ($args[0] -eq $null)
+	{
+		echo "Need Input and Output files"
+	}
+
+	else
+	{
+	pandoc "$param1" -f gfm -t docx --reference-doc='C:\Users\USER\Documents\script\updatetemplate.docx' -s -o "$param2"
+	}
+}
